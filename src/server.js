24e53c5bb96c8976b1ab174a4e5303f66449ae74
Pashka Antonov <pavelcreed222@gmail.com>
2025-11-22 14:04:34 +0200

    auth-04

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { errors } from 'celebrate';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
-import authRoutes from './routes/authRoutes.js.';
+import authRoutes from './routes/authRoutes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
